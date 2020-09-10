2580f6905ebea6c97da60fdb3041c4eb9f0c0d4e
e s <egor@starkov.email>
2020-09-10 14:56:13 +0200

    handoutify: change naming and handout layout

--- lectures/handoutify.sh
+++ lectures/handoutify.sh
@@ -1,9 +1,15 @@
 #/bin/sh
 
-cp $1.pdf $1_raw.pdf
+cp $1.pdf $1_slides.pdf
+echo ----
+echo copied $1.pdf to $1_slides.pdf
 
 # take slide deck filename (without .pdf extension) as input, produce a 2x1 handout.
 # 4:3 version:
-#pdfjam $1.pdf --nup 1x3 --frame true --outfile $1_handout.pdf
-# 16:9 version:
-pdfjam $1.pdf --nup 1x2 --frame true --delta "0 2cm" --noautoscale true --scale 1.2 --outfile $1_handout.pdf
+#pdfjam $1.pdf --nup '1x3' --frame 'true' --outfile $1_handout.pdf
+
+# 16:9 version 1 (1x2):
+#pdfjam $1.pdf --nup '1x2' --frame 'true' --delta "0 2cm" --noautoscale 'true' --scale 1.2 --outfile $1_handout.pdf
+
+# 16:9 version 2 (2x2 landscape):
+pdfjam $1.pdf --nup '2x2' --frame 'true' --landscape --outfile $1_handout.pdf
